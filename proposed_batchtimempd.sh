fc7d221c4b825d145a6479142ebe60758fa262b6
Zdenek Prusa <zdenek.prusa@gmail.com>
2018-09-11 16:11:35 +0200

    More work on the timing script

--- proposed_batchtimempd.sh
+++ proposed_batchtimempd.sh
@@ -4,34 +4,39 @@ MAXIT=200000
 
 for FILE in 39ch1.wav
 do
-bname=(`basename ${FILE%%.*}`)
+    bname=(`basename ${FILE%%.*}`)
 
+    # for SHIFT in 4 8
+    # do
+    #     truncate -s 0 proposed5_${bname}_${SHIFT}.dat
+    #     dictstr=''
+    #     for M in 512 1024 2048 4096 8192
+    #     do
+    #         a=$(( $M/$SHIFT ))
+    #         dictstr+=blackman,$a,$M:
+    #     done
+    #     echo $dictstr >> proposed5_${bname}_${SHIFT}.dat
+    #     ./libltfat/examples/multigabormp/multigabormpd \
+    #     -i $FILE --maxit=$MAXIT -s $SNR -o ${bname}_rec.wav \
+    #     -d $dictstr  \
+    #     > proposed5_${bname}_${SHIFT}.dat
+    #     echo '' >>  proposed5_${bname}_${SHIFT}.dat
+    # done
 
-for SHIFT in 4 8
-do
-    truncate -s 0 proposed5_${bname}_${SHIFT}.dat
-    for M in 512 1024 2048 4096 8192
+    for SHIFT in 4 8 16 32
     do
-        a=$(( $M/$SHIFT )) 
-        ./libltfat/examples/multigabormp/multigabormp -i $FILE -s $SNR -o ${bname}_rec.wav -d blackman,$a,$M   > tmpfile.dat 
-        echo '' >>  proposed5_${bname}_${SHIFT}.dat
+        truncate -s 0 proposed_${bname}_${SHIFT}.dat
+        for M in 512 1024 2048 4096 8192 16384
+        do
+            a=$(( $M/$SHIFT )) 
+            dictstr=blackman,$a,$M
+            echo $dictstr >> proposed_${bname}_${SHIFT}.dat
+            ./libltfat/examples/multigabormp/multigabormpd \
+            -i $FILE --maxit=$MAXIT -s $SNR -o ${bname}_rec.wav \
+            -d $dictstr \
+            >> proposed_${bname}_${SHIFT}.dat
+            echo '' >>  proposed_${bname}_${SHIFT}.dat
+        done
     done
 done
-
-
-# for SHIFT in 4 8 16 32
-# do
-# truncate -s 0 mptk_${bname}_${SHIFT}.dat
-# for M in 512 1024 2048 4096 8192
-# do
-# echo '<?xml version="1.0" encoding="ISO-8859-1"?> <dict> <libVersion>0.2</libVersion>' > tmpdict.xml
-# a=$(( $M/$SHIFT )) 
-# cat blocktemp.xml | sed 's/%M/'"${M}"'/g' | sed 's/%a/'"${a}"'/g' >> tmpdict.xml
-# echo '</dict>' >> tmpdict.xml
-# cat tmpdict.xml
-# ./mptk/bin/mpd --nIter=${MAXIT} -d tmpdict.xml -s $SNR $FILE ${bname}.bin ${bname}_rec.wav  > tmpfile.dat 
-# sed 's/ /'"${M}"' /' tmpfile.dat >> mptk_${bname}_${SHIFT}.dat
-# echo '' >>  mptk_${bname}_${SHIFT}.dat
-# done
-# done
 
